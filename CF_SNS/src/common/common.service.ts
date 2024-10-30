d38aba1bdb8524f722d3177b1e27a8a8ac44a0b9
hugo <werwer748@naver.com>
2024-10-30 19:42:38 +0900

    pagination!!
    일반화하고 공통화하고 상속시키고.. 너무 재밋음 하하하하

--- CF_SNS/src/common/common.service.ts
+++ CF_SNS/src/common/common.service.ts
@@ -1,4 +1,185 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { BasePaginationDto } from './dto/base-pagination.dto';
+import { FindManyOptions, FindOptionsOrder, FindOptionsWhere, Repository } from 'typeorm';
+import { BaseModel } from './entities/base.entity';
+import { FILTER_MAPPER } from './const/filter-mapper.const';
+import { HOST, PROTOCOL } from './const/env.const';
 
 @Injectable()
-export class CommonService {}
+export class CommonService {
+  // 그냥 T로 받아도 되지만 모든 엔티티가 BaseModel을 상속받고 있기때문에
+  // 조금 더 상세한 타입을 잡아주기위해 extends BaseModel을 추가
+  paginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+    path: string
+  ) {
+    if (dto.page) {
+      return this.pagePaginate(
+        dto,
+        repository,
+        overrideFindOptions,
+      )
+    } else {
+      return this.cursorPaginate(
+        dto,
+        repository,
+        overrideFindOptions,
+        path
+      )
+    }
+  }
+
+  private async pagePaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+  ) {
+    const findOptions = this.composeFindOptions<T>(dto);
+
+    const [data, count] = await repository.findAndCount({
+      ...findOptions,
+      ...overrideFindOptions,
+    });
+
+    return {
+      data,
+      total: count,
+    }
+  }
+
+  private async cursorPaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+    path: string
+  ) {
+    const findOptions = this.composeFindOptions<T>(dto);
+
+    const results = await repository.find({
+      ...findOptions,
+      ...overrideFindOptions,
+    })
+
+    const lastItem = results.length === dto.take ? results.at(-1) : null;
+
+    const nextUrl = lastItem && new URL(`${PROTOCOL}://${HOST}/${path}`);
+
+    if (nextUrl) {
+      for (const key of Object.keys(dto)) {
+        if (dto[key]) {
+          if (key !== 'where__id__less_than' && key !== 'where__id__more_than') {
+            nextUrl.searchParams.append(key, dto[key]);
+          }
+        }
+      }
+
+      const key =
+        dto.order__createdAt === 'ASC' ? 'where__id__more_than' : 'where__id__less_than'
+
+      nextUrl.searchParams.append(key, lastItem.id.toString())
+    }
+
+    return {
+      data: results,
+      cursor: {
+        after: lastItem?.id ?? null
+      },
+      count: results.length,
+      next: nextUrl?.toString() ?? null,
+    };
+  }
+
+  private composeFindOptions<T extends BaseModel>(
+    dto: BasePaginationDto,
+  ): FindManyOptions<T> {
+    /**
+     * 해당 함수에서 반환해야하는 값
+     * where,
+     * order,
+     * take,
+     * skip -> page 기반일때만!
+     *
+     * 처리할 로직 정리
+     * 1. where로 시작한다면 필터 로직을 적용한다.
+     * 2. order로 시작한다면 정렬 로직을 적용한다.
+     * 3. 필터 로직을 적용한다면 '__' 기준으로 split 했을 때 값이 3개인지 2개인지 확인한다.
+     *    3-1. 3개로 나뉘면 FILTER_MAPPER에서 해당하는 operator 함수를 찾아서 적용한다.
+     *          ['where', 'id', 'more_than']
+     *    3-2. 2개로 나뉘면 정확한 값을 필터하는 것이기 때문에 operator 없이 적용한다.
+     *          ['where', 'id']
+     * 4. order의 경우 3-2와 같은 방법으로 적용한다.
+     */
+
+    let where: FindOptionsWhere<T> = {}
+    let order: FindOptionsOrder<T> = {}
+
+    for (const [key, value] of Object.entries(dto)) {
+      if (key.startsWith('where__')) {
+        where = {
+          ...where,
+          ...this.parseWhereFilter(key, value),
+        }
+      } else if (key.startsWith('order__')) {
+        order = {
+          ...order,
+          ...this.parseWhereFilter(key, value),
+        }
+      }
+    }
+
+    return {
+      where,
+      order,
+      take: dto.take,
+      skip: dto.page ? dto.take * (dto.page - 1) : null,
+    }
+  }
+
+  private parseWhereFilter<T extends BaseModel>(
+    key: string,
+    value: any
+  ): FindOptionsWhere<T> | FindOptionsOrder<T> {
+    const options: FindOptionsWhere<T> = {};
+
+    const split = key.split('__');
+
+    if (split.length !== 2 && split.length !== 3) {
+      throw new BadRequestException(
+        `where 필터는 '__'로 split 했을때 길이가 2 또는 3이어야 합니다. - ${key}`
+      )
+    }
+
+    // where__id = 3
+    if (split.length === 2) {
+      // ['where', 'id']
+      const [_, field] = split;
+
+      // { id: 3 }
+      options[field] = value;
+    } else {
+      /**
+       * 길이가 3 => Typeorm 유틸리티가 필요
+       * where__id__more_than...
+       *
+       * 3번째 값을 사용하려면?
+       * FILTER_MAPPER에 미리 정의해둔 값들로
+       * field 값에 FILTER_MAPPER에 해당되는 utility를 가져온 후
+       * 값에 적용한다.
+       */
+        // ['where', 'id', 'more_than']
+      const [_, field, operator] = split;
+
+      const values = value.toString().split(',');
+
+      if (operator === 'i_like' || operator === 'like') {
+        options[field] = FILTER_MAPPER[operator](`%${value}%`);
+      } else {
+        options[field] = FILTER_MAPPER[operator](...values);
+      }
+    }
+
+    return options;
+  }
+}
--- CF_SNS/src/main.ts
+++ CF_SNS/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ParseIntPipe, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +12,21 @@ async function bootstrap() {
    *  app.useGlobalPipes(new ValidationPipe())
    *  => class-validator의 검증을 앱에 전역적으로 사용
    */
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // dto에 선언되는 클래스의 기본값을 허용해준다.
+      transform: true,
+      // transform 세부옵션
+      transformOptions: {
+        // validator의 IsNumber등으로 타입을 유추해서 자동으로 타입을 변환시켜 준다.
+        enableImplicitConversion: true,
+      },
+      // 이 옵션을 켜면 validation 데코레이터가 선언되어있지 않은 프로퍼티는 모두 삭제한다.
+      whitelist: true,
+      // 데코레이터로 허용한 프로퍼티가 잘못되었거나 이상한 프로퍼티를 보내면 에러를 발생시킨다.
+      forbidNonWhitelisted: true,
+    })
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
